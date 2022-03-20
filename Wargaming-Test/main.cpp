fe8fd8c8b63ee7d6f523020e769aea87991b1123
Nikita Kozlov <Nyarstot@protonmail.com>
2022-03-20 17:53:11 +0300

    Finish ring_buffer buffer

--- Wargaming-Test/main.cpp
+++ Wargaming-Test/main.cpp
@@ -4,42 +4,8 @@
 
 #include "iseven/iseven.h"
 #include "ring_buffers/ring_buffer.h"
-#include "ring_buffers/dring_buffer.h"
 
 int main()
 {
-	dring_buffer<int> tst(5);
-
-	tst.push(0);
-	tst.push(1);
-	tst.push(2);
-	tst.push(3);
-	tst.push(4);
-	tst.push(5);
-
-	for (dring_buffer<int>::iterator it = tst.begin(); it != tst.end(); it++) {
-		std::cout << *it << " ";
-	}
-
-	/*ring_buffer<int> tmp(5);
-
-	tmp.set_head(0);
-
-	tmp.push_head(0);
-	tmp.push_head(1);
-	tmp.push_head(2);
-	tmp.push_head(3);
-	tmp.push_head(4);
-
-	tmp.push_head(8);
-
-	std::cout << tmp.get_head() << " , " << tmp.get_tail() << "\n";
-
-	std::cout << tmp[0] << " " << tmp[1] << " " << tmp[2] << " " << tmp[3] << " " << tmp[4] << " ";
-
-	/*for (ring_buffer<int>::iterator it = tmp.tail(); it != tmp.head(); ++it) {
-		std::cout << *it << " ";
-	}*/
-
 	return 0;
 }
--- Wargaming-Test/ring_buffers/ring_buffer.h
+++ Wargaming-Test/ring_buffers/ring_buffer.h
@@ -1,158 +1,273 @@
 #ifndef RBUFFER_H
 #define RBUFFER_H
 
-#include <initializer_list>
-#include "ring_iterator.h"
+#include <xmemory>
 
-template<typename T>
+template<typename ring_buffer>
+class ring_iterator
+{
+public:
+	using value_type	= typename ring_buffer::value_type;
+	using index_type	= typename ring_buffer::index_type;
+	using iterator		= typename ring_buffer::iterator;
+
+	using pointer_type		= value_type*;
+	using reference_type	= value_type&;
+
+public:
+	ring_iterator() noexcept {}
+	ring_iterator(pointer_type ptr, pointer_type begin, pointer_type end) noexcept
+		: m_Ptr(ptr), m_Begin(begin), m_End(end)
+	{
+	}
+
+	ring_iterator& operator++()
+	{
+		m_Ptr++;
+		if (m_Ptr >= m_End) {
+			m_Ptr = m_Begin;
+		}
+		return *this;
+	}
+
+	ring_iterator& operator++(int)
+	{
+		ring_iterator it = *this;
+		++(*this);
+		return it;
+	}
+
+	ring_iterator& operator--()
+	{
+		m_Ptr--;
+		if (m_Ptr <= m_Begin) {
+			m_Ptr = m_End;
+		}
+		return *this;
+	}
+
+	ring_iterator& operator--(int)
+	{
+		ring_iterator it = *this;
+		--(*this);
+		return it;
+	}
+
+	reference_type operator[](size_t index) { return *(m_Ptr + index); }
+
+	pointer_type operator->() { return m_Ptr; }
+	reference_type operator*() { return *m_Ptr; }
+
+	bool operator==(const ring_iterator& it) const { return m_Ptr == it.m_Ptr; }
+	bool operator!=(const ring_iterator& it) const { return !(*this == it); }
+
+private:
+	pointer_type m_Ptr = nullptr;
+
+	pointer_type m_Begin = nullptr;
+	pointer_type m_End = nullptr;
+};
+
+template<typename _Ty, class _Alloc = std::allocator<_Ty>>
 class ring_buffer
 {
 public:
-	using value_type	= T;
-	using iterator		= ring_iterator<ring_buffer<T>>;
+	using value_type = _Ty;
+	using index_type = size_t;
+
+	using _Alty = _Alloc;
+	using _Alty_traits = std::allocator_traits<_Alty>;
+
+	using iterator = ring_iterator<ring_buffer<_Ty,_Alty>>;
 
 public:
-	ring_buffer(size_t size)
-		: m_Size(size), m_Capacity(size + 1)
+	ring_buffer() noexcept
 	{
-		m_Buffer = new T[m_Capacity];
+		m_Buffer = nullptr;
+
+		m_Size = 0;
+		m_Capacity = 0;
+
+		m_Head = 0;
+		m_Tail = 0;
+
+		m_Alloc = _Alty;
 	}
-	ring_buffer(size_t size, size_t index)
-		: m_Size(size), m_Capacity(size + 1), m_Head(index), m_Tail(index)
+
+	explicit ring_buffer(size_t size, const _Alty& allocator = _Alty())
 	{
-		m_Buffer = new T[m_Capacity];
-		set_head(index);
+		m_Head = 0;
+		m_Tail = 0;
+
+		m_Size = size;
+		m_Capacity = size + 1;
+
+		m_Alloc = allocator;
+
+		m_Buffer = m_Alloc.allocate(m_Capacity);
+		set_to_null(m_Buffer, m_Size);
 	}
+
 	~ring_buffer()
 	{
 		clear();
-		::operator delete(m_Buffer, m_Capacity * sizeof(T));
+		m_Alloc.deallocate(m_Buffer, m_Capacity);
 	}
 
-	void push_head(const T& value)
+	void push(const value_type& value)
 	{
+		if (full()) {
+			throw std::logic_error("Buffer is full now");
+		}
+
+		m_Counter++;
 		m_Buffer[m_Head] = std::move(value);
-
-		++m_Head;
-		if (m_Head == m_Size) {
-			m_Head = 0;
-		}
-
-		if (m_Head == m_Tail) {
-			m_Tail++;
-			if (m_Tail == m_Size) {
-				m_Tail = 0;
-			}
-		}
+		m_Head = next_index(m_Head, m_Size);
 	}
 
-	void push_tail(const T& value)
+	template <typename... _Va>
+	void emplace(_Va&&... args)
 	{
-		m_Buffer[m_Tail] = std::move(value);
-
-		--m_Tail;
-		if (m_Tail == 0) {
-			m_Tail = m_Size - 1;
+		if (full()) {
+			throw std::logic_error("Buffer is full now");
 		}
 
-		if (m_Tail == m_Head) {
-			--m_Head;
-			if (m_Head == 0) {
-				m_Head = m_Size - 1;
-			}
+		m_Counter++;
+		_Alty_traits::construct(m_Alloc, m_Buffer + m_Head, std::forward<_Va>(args)...);
+		m_Head = next_index(m_Head, m_Size);
+	}
+
+	void pop()
+	{
+		if (empty()) {
+			throw std::logic_error("Biffer is empty now");
 		}
+
+		m_Counter--;
+		m_Buffer[m_Tail] = 0;
+		m_Tail = next_index(m_Tail, m_Size);
+	}
+
+	value_type pop_return()
+	{
+		if (empty()) {
+			throw std::logic_error("Biffer is empty now");
+		}
+
+		value_type tmp = std::move(m_Buffer[m_Tail]);
+
+		m_Counter--;
+		m_Buffer[m_Tail] = 0;
+		m_Tail = next_index(m_Tail, m_Size);
+
+		return tmp;
 	}
 
 	void clear()
 	{
-		for (size_t i{}; i != m_Size; i++) {
-			m_Buffer[i].~T();
+		if (m_Buffer == nullptr) { return; }
+
+		for (size_t i{}; i < m_Size; i++) {
+			_Alty_traits::destroy(m_Alloc, m_Buffer + i);
 		}
 
-		m_Size = 0;
 		m_Head = 0;
+		m_Tail = 0;
+
+		m_Counter = 0;
+		m_Size = 0;
+	}
+
+	void resize(size_t new_size)
+	{
+		if (new_size == m_Size) {
+			return;
+		}
+
+		value_type* new_Buffer = m_Alloc.allocate(new_size + 1);
+		set_to_null(new_Buffer, new_size);
+
+		for (size_t i = m_Tail, c{}; c != m_Size; i = next_index(i, m_Size), c++) {
+			new_Buffer[c] = m_Buffer[i];
+		}
+
+		if (m_Buffer != nullptr) {
+			for (size_t i{}; i < m_Size; i++) {
+				_Alty_traits::destroy(m_Alloc, m_Buffer + i);
+			}
+			m_Alloc.deallocate(m_Buffer, m_Size);
+		}
+
+		m_Buffer = new_Buffer;
+
+		m_Size = new_size;
+		m_Capacity = new_size + 1;
+
+		m_Head = m_Counter;
 		m_Tail = 0;
 	}
 
-	void set_head(size_t index)
+	void insert(iterator lhead ,iterator ltail, iterator rhead)
 	{
-		if (index > m_Size) {
-			throw std::invalid_argument("Index must be less than ring size");
+		for (ltail; ltail != lhead; ltail++) {
+			*rhead = *ltail;
+			rhead++;
 		}
-
-		m_Head = index;
-	}
-
-	size_t get_head()
-	{
-		return m_Head;
-	}
-
-	void set_tail(size_t index)
-	{
-		if (index > m_Size) {
-			throw std::invalid_argument("Index must be less than ring size");
-		}
-
-		m_Tail = index;
-	}
-
-	size_t get_tail()
-	{
-		return m_Tail;
-	}
-
-	bool empty()
-	{
-		return m_Tail == m_Head;
-	}
-
-	bool full()
-	{
-		return m_Head++ == m_Tail;
-	}
-
-	size_t size()
-	{
-		return m_Size;
-	}
-
-	size_t capacity()
-	{
-		return m_Capacity;
 	}
 
 	iterator head()
 	{
-		return iterator(m_Buffer + m_Head, m_Size,
-						m_Buffer, m_Buffer + m_Size);
+		return iterator(m_Buffer + m_Head, 
+				m_Buffer, m_Buffer + m_Size);
 	}
 
 	iterator tail()
 	{
-		return iterator(m_Buffer + m_Tail, m_Size,
-						m_Buffer, m_Buffer + m_Size);
+		return iterator(m_Buffer + m_Tail, 
+				m_Buffer, m_Buffer + m_Size);
 	}
 
-	const T& operator[](size_t index) const
-	{
-		return m_Buffer[index];
-	}
+	bool empty() const { return m_Counter == 0; }
+	bool full() const { return m_Counter == m_Size; }
 
-	T& operator[](size_t index)
-	{
-		return m_Buffer[index];
-	}
+	size_t size() const { return m_Size; }
+	size_t capacity() const { return m_Capacity; }
+	size_t get_head() const { return m_Head; }
+	size_t get_tail() const { return m_Tail; }
+
+	const value_type& operator[](index_type index) const { return m_Buffer[index]; }
+	value_type& operator[](index_type index) { return m_Buffer[index]; }
 
 private:
-	T* m_Buffer			= nullptr;
+	value_type* m_Buffer	= nullptr;
 
-	size_t m_Size		= 0;
-	size_t m_Capacity	= 0;
+	size_t m_Size			= 0;
+	size_t m_Capacity		= 0;
+	size_t m_Counter		= 0;
 
-	size_t m_Head		= 0;
-	size_t m_Tail		= 0;
+	index_type m_Head		= 0;
+	index_type m_Tail		= 0;
 
+	_Alty m_Alloc;
+
+private:
+	_NODISCARD static index_type next_index(index_type index, index_type size_index) noexcept
+	{
+		index++;
+		return index >= size_index ? 0 : index;
+	}
+
+	_NODISCARD static index_type prev_index(index_type index, index_type size_index) noexcept
+	{
+		return index == 0 ? (size_index - 1) : index--;
+	}
+
+	void set_to_null(value_type* buffer, size_t sze) noexcept
+	{
+		for (size_t i{}; i < sze; i++) {
+			buffer[i] = NULL;
+		}
+	}
 };
 
 #endif // !RBUFFER_H
